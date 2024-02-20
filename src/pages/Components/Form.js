051d401fce86199b9cee1d461d783f264f3b5e3d
Anthony <zangler46@gmail.com>
2024-02-20 11:33:22 -0600

    templateUpdate

--- src/pages/Components/Form.js
+++ src/pages/Components/Form.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import { Grid, TextField } from "@mui/material";
+import { FaStar } from "react-icons/fa";
 
 const Form = () => {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -20,7 +25,29 @@ const Form = () => {
         <TextField required fullWidth id="dormName" label="Dorm Name" />
       </Grid>
       <Grid item xs={12}>
-        <TextField required fullWidth id="stars" label="X out of 5 stars"/>
+        {[...Array(5)].map((star, index) => {
+          const currentRating = index + 1;
+          return (
+            <label>
+              <input
+                type="radio"
+                name="rating"
+                value={currentRating}
+                onClick={() => setRating(currentRating)}
+              />
+              <FaStar
+                className="star"
+                size={40}
+                color={
+                  currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9"
+                }
+                onMouseEnter={() => setHover(currentRating)}
+                onMouseLeave={() => setHover(null)}
+              />
+            </label>
+          );
+        })}
+        <p>Your rating is {rating}</p>
       </Grid>
     </Grid>
   );
