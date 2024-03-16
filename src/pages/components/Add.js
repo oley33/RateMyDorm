059555d5ccbe7c343ccfcbf2115d6854bb1efe4f
Hayley Herlich <herhm-24@rhodes.edu>
2024-03-16 10:40:23 -0500

    add modal ready for tony's content

--- src/pages/components/Add.js
+++ src/pages/components/Add.js
@@ -1,37 +1,57 @@
-import { Box, Fab, IconButton, Modal, Tooltip } from '@mui/material';
+import { Fab, Grid, Modal, Tooltip } from '@mui/material';
 import React, { useReducer, useState } from 'react';
 import { Add as AddIcon } from "@mui/icons-material";
-import {reducerExample, initialState} from "./reducerExample";
-
+import { reducerExample, initialState } from "./reducerExample";
 
 const Add = () => {
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
 
-    const [state, dispatch] = useReducer(reducerExample, initialState)
+    const [state, dispatch] = useReducer(reducerExample, initialState);
+
     return (
         <>
-            <Tooltip onClick={e=>setOpen(true)}
-            title="Delete" 
-            sx={{ position: "fixed", 
-            bottom: 20, 
-            left: { xs: "calc(50%-25px)", md: 30 } }}>
+            <Tooltip onClick={() => setOpen(true)}
+                title="Delete"
+                sx={{
+                    position: "fixed",
+                    bottom: 20,
+                    left: { xs: "calc(50%-25px)", md: 30 }
+                }}
+            >
                 <Fab color="primary" aria-label="add">
                     <AddIcon />
-
                 </Fab>
             </Tooltip>
             <Modal
                 open={open}
-                onClose={e=>setOpen(false)}
+                onClose={() => setOpen(false)}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box >
-                    Hello
-                </Box>
+                <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    sx={{
+                        position: 'absolute',
+                        width: 600, // Adjust as needed
+                        height: 600, // Adjust as needed
+                        backgroundColor: 'white',
+                        borderRadius: 4,
+                        boxShadow: 24,
+                        p: 4,
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                    }}
+                >
+                    <Grid item>
+                        Tony's Post Content Goes Here
+                    </Grid>
+                </Grid>
             </Modal>
         </>
-    )
+    );
 }
 
-export default Add
+export default Add;
