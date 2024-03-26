35dcadfc4a7066e58d213ce06cc83e3bd0288404
Anthony <zangler46@gmail.com>
2024-03-26 12:12:22 -0500

    before changing DormSearch

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Post from './pages/Post'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Post from "./pages/Post";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+const queryClient = new QueryClient();
 
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/post" element={<Post />} />
-
-    </Routes>
-  </Router>,
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/post" element={<Post />} />
+      </Routes>
+    </Router>
+  </QueryClientProvider>,
 
   document.getElementById("root")
-
 );
 
 serviceWorker.unregister();
-
-
--- src/pages/Components/Form.js
+++ src/pages/Components/Form.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { useState } from "react";
-import { Grid, TextField } from "@mui/material";
+import { FormControlLabel, FormGroup, Grid, TextField, Checkbox } from "@mui/material";
 import { FaStar } from "react-icons/fa";
+import DormSearch from "./DormSearch";
 
 const Form = () => {
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
+
+  const initialValues = {
+    FirstNameValue: '',
+    LastNameValue: '',
+    Email: '',
+    Review: '',
+    
+  };
+  const [values, setValues] = useState(initialValues);
+
+  const handleTextInputChange = event => {
+    setValues({...values, [event.target.name]: event.target.value,});
+  };
 
   return (
     <Grid container spacing={2}>
@@ -13,18 +27,20 @@ const Form = () => {
         <h2 style={{ textAlign: "center" }}>Review</h2>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <TextField required fullWidth id="firstName" label="First Name" />
+        <TextField required fullWidth id="firstName" label="First Name" name="FirstNameValue" value={values.FirstNameValue} onChange={handleTextInputChange} />
       </Grid>
       <Grid item xs={12} sm={6}>
-        <TextField required fullWidth id="lastName" label="Last Name" />
+        <TextField required fullWidth id="lastName" label="Last Name" name="LastNameValue" value={values.LastNameValue}  onChange={handleTextInputChange}/>
       </Grid>
       <Grid item xs={12}>
-        <TextField required fullWidth id="email" label="Email" />
+        <TextField required fullWidth id="email" label="Email" name="Email" value={values.Email} onChange={handleTextInputChange}/>
       </Grid>
       <Grid item xs={12}>
-        <TextField required fullWidth id="dormName" label="Dorm Name" />
+        <DormSearch/>
       </Grid>
       <Grid item xs={12}>
+        
+        <p>Overall rating: {rating}</p>
         {[...Array(5)].map((star, index) => {
           const currentRating = index + 1;
           return (
@@ -47,9 +63,29 @@ const Form = () => {
             </label>
           );
         })}
-        <p>Your rating is {rating}</p>
       </Grid>
+      <Grid item xs={12}>
+        <TextField fullWidth multiline id="review" label="Please enter your review here" name="Review" value={values.Review} onChange={handleTextInputChange}/>
+      </Grid>
+      <Grid item xs={12}>
+        <p>Check which options apply:</p>
+        <FormGroup>
+          <FormControlLabel control={<Checkbox />} label="Clean" />
+          <FormControlLabel control={<Checkbox />} label="Quiet" />
+          <FormControlLabel control={<Checkbox />} label="Loud" />
+          <FormControlLabel control={<Checkbox />} label="Good Location" />
+          <FormControlLabel control={<Checkbox />} label="Bugs" />
+          <FormControlLabel control={<Checkbox />} label="Bug Free" />
+        </FormGroup>
+      </Grid>
+      <p>Check input value: {values.FirstNameValue}</p>
+      <p>Last name: {values.LastNameValue}</p>
+      <p>Email: {values.Email}</p>
+      <p>rating: {rating}</p>
+      <p>Data from dorm: {}</p>
+      <p>{values.Review}</p>
     </Grid>
+    
   );
 };
 
